3dfb7c414724218f25405208d9c198a3b0c5f47b
tomo <103444870+tomo2@users.noreply.github.com>
2023-08-15 23:35:48 +0900

    変数を格納する変数を作成

--- calendar.php
+++ calendar.php
@@ -1,10 +1,24 @@
+<?php
+// タイムゾーンの設定
+date_default_timezone_set('Asia/Tokyo');
+
+$today = filter_input(INPUT_POST, 'today');
+$ = filter_input(INPUT_POST, 'monthNext');
+$ = filter_input(INPUT_POST, 'yearNext');
+$ = filter_input(INPUT_POST, 'monthPrev');
+$ = filter_input(INPUT_POST, 'yearPrev');
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>calendar</title>
+    <title>カレンダー</title>
 </head>
 <body>
     
