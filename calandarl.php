240e5baf4d95a2eedc95dfd12e9c24751b486f9b
tomo <103444870+tomo2@users.noreply.github.com>
2023-08-18 00:51:07 +0900

    カレンダーの作成

--- calandarl.php
+++ calandarl.php
@@ -5,8 +5,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>カレンダー</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
+<table>
+    <tr class="day">
+        <th>日</th>
+        <th>月</th>
+        <th>火</th>
+        <th>水</th>
+        <th>木</th>
+        <th>金</th>
+        <th>土</th>
+    </tr>
+
+    <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+        <td>7</td>
+        <td>8</td>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+        <td>7</td>
+        <td>8</td>
+    </tr>
+</table>
 </body>
 </html>
