0cd48fa14f12b556af5579fb567a36a593d69025
tomo <103444870+tomo2@users.noreply.github.com>
2023-08-22 23:50:23 +0900

    HTMLの数字の消去

--- calandar.php
+++ calandar.php
@@ -93,13 +93,6 @@ echo '</pre>';
                     
                 }
                 ?>
-                <td>1</td>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>4</td>
-                <td>5</td>
-                
             </tr>
 
         </table>
