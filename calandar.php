a7fed778d2b82e1d27490b39c4b619822b6f1a01
tomo <103444870+tomo2@users.noreply.github.com>
2023-08-22 23:49:48 +0900

    改行の処理

--- calandar.php
+++ calandar.php
@@ -22,6 +22,9 @@ $week = ["日", '月', '火', '水', '木', '金', '土'];
 $firstWeek = date('w', $firstDay);
 $lastWeek = date('w', $lastDay);
 
+
+// $oneWeek = mktime()
+
 // 空の配列の準備
 $nunbers = [];
 
@@ -81,11 +84,11 @@ echo '</pre>';
             
                 // カレンダーの呼び出し
                 foreach ($oneMonth as $oneMonths) {
-
-                    if ($oneMonths === 6) {
-                    echo '<tr>' . '<td>' . $oneMonths . '</td>' . '</tr>';
+                    
+                    echo'<td>' . $oneMonths . '</td>';
+                    if ($oneMonths % 7 == 6) {
+                        echo '</tr>';
                     }
-                    echo '<td>' . $oneMonths . '</td>';
 
                     
                 }
@@ -98,42 +101,6 @@ echo '</pre>';
                 <td>5</td>
                 
             </tr>
-            <tr>
-                <td>6</td>
-                <td>7</td>
-                <td>8</td>
-                <td>9</td>
-                <td>10</td>
-                <td>11</td>
-                <td>12</td>
-            </tr>
-            <tr>
-                <td>13</td>
-                <td>14</td>
-                <td>15</td>
-                <td>16</td>
-                <td>17</td>
-                <td>18</td>
-                <td>19</td>
-                
-            </tr>
-            <tr>
-                <td>20</td>
-                <td>21</td>
-                <td>22</td>
-                <td>23</td>
-                <td>24</td>
-                <td>25</td>
-                <td>26</td>
-                
-            </tr>
-            <tr>
-                <td>27</td>
-                <td>28</td>
-                <td>29</td>
-                <td>30</td>
-                <td>31</td>
-                <td></td>
 
         </table>
     </div>
