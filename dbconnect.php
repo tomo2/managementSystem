6283c86b7bd563347bfc0762c3fce76626b1eb73
tomo <103444870+tomo2@users.noreply.github.com>
2023-08-26 03:25:21 +0900

    Add files via upload

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,23 @@
 <?php
 
-$host = DB_HOST;
-$db = DB_NAME;
-$user = DB_USER;
-$pass = DB_PASS;
+$host = "localhost";
+$db = "データベース名を入れる";
+$user = "root";
+$pass = "root";
 
 $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
 
-$pdo = new PDO($dsn, $user, $pass, [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-]);
+try {
+    
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+    echo '接続成功です';
+    return $pdo;
+
+} catch (PDOException $e) {
+    echo '接続失敗です！' . $e->getMessage();
+    exit();
+}
