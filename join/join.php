4e6bb53851dfc874ee67e8cd3b0872271db3b603
tomo <103444870+tomo2@users.noreply.github.com>
2023-08-28 23:53:59 +0900

    入力画面の作成

---
+++ join/join.php
@@ -0,0 +1,40 @@
+<?
+
+$form['name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_ADD_SLASHES);
+if ($form['name'] === '') {
+    echo '名前を入力してください';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>利用者登録画面</title>
+</head>
+<body>
+
+<h2>ユーザー登録を行う</h2>
+
+    <form action="" method="post">
+        <p>
+            <label for="name">ユーザー名：</label>
+            <input type="text" name="name">
+        </p>
+        <p>
+            <label for="password">パスワード：</label>
+            <input type="password" name="password">
+        </p>
+        <p>
+            <label for="password_conf">パスワード確認</label>
+            <input type="password" name="password_conf">
+        </p>
+        <p>
+            <input type="submit" value="新規登録">
+        </p>
+    </form>
+    
+</body>
+</html>
