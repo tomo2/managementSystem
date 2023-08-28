27fa066e43ba283c8a0b19df6feeade011db2333
tomo <103444870+tomo2@users.noreply.github.com>
2023-08-28 16:38:53 +0900

    POSTで登録内容を送信する

---
+++ signup/signup.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ユーザー登録</title>
+</head>
+
+<body>
+
+    <h1>ユーザー登録フォーム</h1>
+    <form action="register.php" method="POST">
+        
+    </form>
+
+</body>
+</html>
