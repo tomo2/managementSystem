c61890dc7fe38412377f08a9bfbc9fa45b7ced88
tomo <103444870+tomo2@users.noreply.github.com>
2023-09-07 23:36:37 +0900

    ボタンの修正

--- registration/registration.php
+++ registration/registration.php
@@ -17,17 +17,16 @@
                 <label><input type="radio" name="care">要介護3<br></label>
                 <label><input type="radio" name="care">要介護4<br></label>
                 <label><input type="radio" name="care">要介護5<br></label>
-
-        ・時間<br>
-        <label><input type="radio" name="care">3~4時間<br></label>
-        <label><input type="radio" name="care">4~5時間<br></label>
-        <label><input type="radio" name="care">5~6時間<br></label>
-        <label><input type="radio" name="care">6~7時間<br></label>
-        <label><input type="radio" name="care">7~8時間<br></label>
-        <label><input type="radio" name="care">8~9時間<br></label>
-
-
-
+    </form>
+                
+    <form action="" method="post">
+                    ・時間<br>
+                    <label><input type="radio" name="time">3~4時間<br></label>
+                    <label><input type="radio" name="time">4~5時間<br></label>
+                    <label><input type="radio" name="time">5~6時間<br></label>
+                    <label><input type="radio" name="time">6~7時間<br></label>
+                    <label><input type="radio" name="time">7~8時間<br></label>
+                    <label><input type="radio" name="time">8~9時間<br></label>
     </form>
     
 </body>
