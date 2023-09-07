9041d556e9396c32db2c0d1b4b36b668e86fb7c0
tomo <103444870+tomo2@users.noreply.github.com>
2023-09-07 22:34:52 +0900

    介護度の作成

--- registration/registration.php
+++ registration/registration.php
@@ -9,7 +9,15 @@
 <body>
 
     <form action="" method="post">
-        
+        お名前：<input type="text" name="name"><br>
+
+        度合い：<label><input type="checkbox" name="care">要介護1<br></label>
+                <label><input type="checkbox" name="care">要介護2<br></label>
+                <label><input type="checkbox" name="care">要介護3<br></label>
+                <label><input type="checkbox" name="care">要介護4<br></label>
+                <label><input type="checkbox" name="care">要介護5<br></label>
+
+        時間：<input type="radio">
 
     </form>
     
