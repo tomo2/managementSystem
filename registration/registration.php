3c1c2950226761fd97b823ca9d87d6b4cee7bad3
tomo <103444870+tomo2@users.noreply.github.com>
2023-09-07 22:15:47 +0900

    HTMLの作成

--- registration/registration.php
+++ registration/registration.php
@@ -1,1 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>利用者登録ページ</title>
+</head>
+<body>
 
+    <form action="" method="post">
+        
+
+    </form>
+    
+</body>
+</html>
