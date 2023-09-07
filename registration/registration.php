77f5872c8542f5c6d87f3cdaf5eb943e1e9b7bba
tomo <103444870+tomo2@users.noreply.github.com>
2023-09-07 22:41:48 +0900

    ボタンの作成

--- registration/registration.php
+++ registration/registration.php
@@ -11,13 +11,22 @@
     <form action="" method="post">
         お名前：<input type="text" name="name"><br>
 
-        度合い：<label><input type="checkbox" name="care">要介護1<br></label>
-                <label><input type="checkbox" name="care">要介護2<br></label>
-                <label><input type="checkbox" name="care">要介護3<br></label>
-                <label><input type="checkbox" name="care">要介護4<br></label>
-                <label><input type="checkbox" name="care">要介護5<br></label>
+        ・度合い<br>
+                <label><input type="radio" name="care">要介護1<br></label>
+                <label><input type="radio" name="care">要介護2<br></label>
+                <label><input type="radio" name="care">要介護3<br></label>
+                <label><input type="radio" name="care">要介護4<br></label>
+                <label><input type="radio" name="care">要介護5<br></label>
 
-        時間：<input type="radio">
+        ・時間<br>
+        <label><input type="radio" name="care">3~4時間<br></label>
+        <label><input type="radio" name="care">4~5時間<br></label>
+        <label><input type="radio" name="care">5~6時間<br></label>
+        <label><input type="radio" name="care">6~7時間<br></label>
+        <label><input type="radio" name="care">7~8時間<br></label>
+        <label><input type="radio" name="care">8~9時間<br></label>
+
+
 
     </form>
     
