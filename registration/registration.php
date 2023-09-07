5ce76d8ca643cfb2647fa52ec23791950dd99dce
tomo <103444870+tomo2@users.noreply.github.com>
2023-09-07 23:55:24 +0900

    postでradioボタンの値を受け取る

--- registration/registration.php
+++ registration/registration.php
@@ -1,3 +1,11 @@
+<?php
+
+if (isset($_POST['care'])) {
+    echo $_POST['care'];
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -12,21 +20,21 @@
         お名前：<input type="text" name="name"><br>
 
         ・度合い<br>
-                <label><input type="radio" name="care">要介護1<br></label>
+                <label><input type="radio" name="care" value="3-4">要介護1<br></label>
                 <label><input type="radio" name="care">要介護2<br></label>
                 <label><input type="radio" name="care">要介護3<br></label>
                 <label><input type="radio" name="care">要介護4<br></label>
                 <label><input type="radio" name="care">要介護5<br></label>
-    </form>
                 
-    <form action="" method="post">
-                    ・時間<br>
-                    <label><input type="radio" name="time">3~4時間<br></label>
-                    <label><input type="radio" name="time">4~5時間<br></label>
-                    <label><input type="radio" name="time">5~6時間<br></label>
-                    <label><input type="radio" name="time">6~7時間<br></label>
-                    <label><input type="radio" name="time">7~8時間<br></label>
-                    <label><input type="radio" name="time">8~9時間<br></label>
+                ・時間<br>
+                <label><input type="radio" name="time">3~4時間<br></label>
+                <label><input type="radio" name="time">4~5時間<br></label>
+                <label><input type="radio" name="time">5~6時間<br></label>
+                <label><input type="radio" name="time">6~7時間<br></label>
+                <label><input type="radio" name="time">7~8時間<br></label>
+                <label><input type="radio" name="time">8~9時間<br></label>
+
+                <button type="submit">送信する</button>
     </form>
     
 </body>
