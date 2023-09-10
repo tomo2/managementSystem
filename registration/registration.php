11fbfd07ed352ecdf89d056228d9ee50b6e92859
tomo <103444870+tomo2@users.noreply.github.com>
2023-09-10 23:29:36 +0900

    送信する値の調整

--- registration/registration.php
+++ registration/registration.php
@@ -22,19 +22,19 @@ if (isset($_POST['care'])) {
         お名前：<input type="text" name="name"><br>
 
         ・度合い<br>
-                <label><input type="radio" name="care" value="1">要介護1<br></label>
-                <label><input type="radio" name="care" value="2">要介護2<br></label>
-                <label><input type="radio" name="care" value="3">要介護3<br></label>
-                <label><input type="radio" name="care" value="4">要介護4<br></label>
-                <label><input type="radio" name="care" value="5">要介護5<br></label>
+                <label><input type="radio" name="care" value="要介護1">要介護1<br></label>
+                <label><input type="radio" name="care" value="要介護2">要介護2<br></label>
+                <label><input type="radio" name="care" value="要介護3">要介護3<br></label>
+                <label><input type="radio" name="care" value="要介護4">要介護4<br></label>
+                <label><input type="radio" name="care" value="要介護5">要介護5<br></label>
                 
                 ・時間<br>
-                <label><input type="radio" name="time" value="3">3~4時間<br></label>
-                <label><input type="radio" name="time" value="4">4~5時間<br></label>
-                <label><input type="radio" name="time" value="5">5~6時間<br></label>
-                <label><input type="radio" name="time" value="6">6~7時間<br></label>
-                <label><input type="radio" name="time" value="7">7~8時間<br></label>
-                <label><input type="radio" name="time" value="8">8~9時間<br></label>
+                <label><input type="radio" name="time" value="3~4時間">3~4時間<br></label>
+                <label><input type="radio" name="time" value="4~5時間">4~5時間<br></label>
+                <label><input type="radio" name="time" value="5~6時間">5~6時間<br></label>
+                <label><input type="radio" name="time" value="6~7時間">6~7時間<br></label>
+                <label><input type="radio" name="time" value="7~8時間">7~8時間<br></label>
+                <label><input type="radio" name="time" value="8~9時間">8~9時間<br></label>
 
                 <button type="submit">送信する</button>
     </form>
