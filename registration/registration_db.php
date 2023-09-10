b830f0cf0f3f39095c8a3549cb179fcb5c55fe69
tomo <103444870+tomo2@users.noreply.github.com>
2023-09-10 23:22:09 +0900

    コメント記入

--- registration/registration_db.php
+++ registration/registration_db.php
@@ -4,20 +4,25 @@
 // 送信された値を受け取る
 if (isset($_POST['name'])) {
 
+    // 受け取った値を変数に入れる
     $name = htmlspecialchars($_POST['name'], ENT_QUOTES);
     $care = htmlspecialchars($_POST['care'], ENT_QUOTES);
     $time = htmlspecialchars($_POST['time'], ENT_QUOTES);
 
-
+    // データベース接続
     $dsn = "mysql:host=localhost;dbname=msystem;charset=utf8";
     $user="root";
     $pass="root";
 
+    // 受け取った値をデータベースに保存する
     try {
         $db = new PDO($dsn, $user, $pass);
-        $db->query("INSERT INTO msistem (name, care, time)
+        $db->query("INSERT INTO users (name, care, time)
                     VALUES ('$name', '$care', '$time')");
+                    echo 'ログインが成功しました';
     }catch (Exception $e) {
         echo $e->getMessage();
     }
+
+    
 }
