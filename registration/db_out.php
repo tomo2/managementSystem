c0b6e35d250fc49232912fb5f61169aaf08914de
tomo <103444870+tomo2@users.noreply.github.com>
2023-09-11 22:16:46 +0900

    dbの中身の取り出し

--- registration/db_out.php
+++ registration/db_out.php
@@ -1,3 +1,23 @@
 <?php
 
+    // データベース接続
+    $dsn = "mysql:host=localhost;dbname=msystem;charset=utf8";
+    $user="root";
+    $pass="root";
+
+    // 受け取った値をデータベースに保存する
+    try {
+        $db = new PDO($dsn, $user, $pass);
+
+        $stmt = $db->prepare("SELECT * FROM users");
+
+        $stmt->execute();
+        
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        var_dump($result);
+
+    }catch (Exception $e) {
+        echo $e->getMessage();
+    }
+
 ?>
