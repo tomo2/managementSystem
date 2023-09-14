fc8622888bc5da8a9f1d2c201753ddc283a3f2dd
tomo <103444870+tomo2@users.noreply.github.com>
2023-09-14 22:32:49 +0900

    cssの場所の変更

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
-
 require './registration/db_out.php';
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+
+<?php
 require 'calendar.php';
