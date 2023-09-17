40ff22e4734d540eaa6716877d09343b2eb6bc45
tomo <103444870+tomo2@users.noreply.github.com>
2023-09-17 22:20:23 +0900

    カレンダーへの出力の処理。うまくいかない。

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let chocho = document.getElementById('name1');
+
+let karen = document.getElementById('09.10');
+
+console.log(chocho);
+console.log(karen);
+
+
+karen.textContent = chocho;
+
+
